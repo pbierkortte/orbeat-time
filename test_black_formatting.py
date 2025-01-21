a0645491407d082b8bcc3dbe8dd84bbbf15d6265
Pat <24831708+pbierkortte@users.noreply.github.com>
2025-01-21 15:08:44 -0500

    remove black pytest

--- test_black_formatting.py
+++
@@ -1,6 +0,0 @@
-from black.cli import main
-
-
-def test_black_formatting():
-    """Test that all Python files are formatted according to black standards."""
-    black.main(["--check", "--line-length=120", "."])
